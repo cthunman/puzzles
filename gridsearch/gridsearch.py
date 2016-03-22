722daa1aee2caa91c86b70aa6b64d3d9b9d6f174
Carl <thunman@gmail.com>
2016-03-21 20:03:33 -0400

    mostly working... need to find edge cases and stuff

--- gridsearch/gridsearch.py
+++ gridsearch/gridsearch.py
@@ -16,55 +16,57 @@ def find_string_indeces(find_string, search_string):
 
 t = int(raw_input().strip())
 for a0 in xrange(t):
-    R,C = raw_input().strip().split(' ')
-    R,C = [int(R),int(C)]
-    G = []
-    G_i = 0
-    for G_i in xrange(R):
-        G_t = str(raw_input().strip())
-        G.append(G_t)
-    r,c = raw_input().strip().split(' ')
-    r,c = [int(r),int(c)]
-    P = []
-    P_i = 0
-    for P_i in xrange(r):
-        P_t = str(raw_input().strip())
-        P.append(P_t)
+  R,C = raw_input().strip().split(' ')
+  R,C = [int(R),int(C)]
+  G = []
+  G_i = 0
+  for G_i in xrange(R):
+    G_t = str(raw_input().strip())
+    G.append(G_t)
+  r,c = raw_input().strip().split(' ')
+  r,c = [int(r),int(c)]
+  P = []
+  P_i = 0
+  for P_i in xrange(r):
+    P_t = str(raw_input().strip())
+    P.append(P_t)
 
-    print 'G'
-    print G
-    print 'P'
-    print P
+  # print 'G'
+  # for row in G:
+  #   print row
+  # print 'P'
+  # for row in P:
+  #   print row
 
-    match_dict = OrderedDict()
-    for g_i, g in enumerate(G):
-      for p_i, p in enumerate(P):
-        match_dict[(g_i, p_i)] = find_string_indeces(p, g)
-        # match_dict[search_row, find_row] : char_index
+  match_dict = OrderedDict()
+  index_dict = OrderedDict()
+  for g_i, g in enumerate(G):
+    for p_i, p in enumerate(P):
+      match_dict[(g_i, p_i)] = find_string_indeces(p, g)
+      for i in find_string_indeces(p, g):
+        if i not in index_dict:
+          index_dict[i] = []
+        index_dict[i].append((g_i, p_i))
 
-    # print match_dict
-    for key in match_dict:
-      if len(match_dict[key]) == 0:
-        del match_dict[key]
-        
-    rows = set()
-    input_rows = set()
+  target_len = len(P)
+  is_matched = False
+  counter = 0
+  last_row_index = -1
+  for i in index_dict:
+    for v in index_dict[i]:
+      if v[1] == 0:
+        last_row_index = v[0]
+        counter = 1
+      elif v[1] == counter:
+        if last_row_index == v[0] - 1: 
+          last_row_index = v[0] 
+          counter = counter + 1
+          if counter == target_len - 1:
+            is_matched = True
+      else:
+        counter = 0
 
-    target_len = len(p)
-    print 'target_len'
-    print target_len
-
-    row_counter = {}
-    for key in match_dict:
-      rows.add(key[0])
-      input_rows.add(key[1])
-
-      if match_dict[key]
-
-      
-
-print find_string_indeces('def', 'abcdefghijk')
-print find_string_indeces('def', 'abcdefghijkdef')
-print match_dict
-print rows
-print input_rows
+  if is_matched:
+    print 'YES'
+  else:
+    print 'NO'
